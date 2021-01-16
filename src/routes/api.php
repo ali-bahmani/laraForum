6e49b204d341e1321e60df06c24ae470075c8e19
ali-bahmani <alibahmani0@gmail.com>
2021-01-16 03:37:57 +0330

    add authentication login + register + logout

--- src/routes/api.php
+++ src/routes/api.php
@@ -14,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+
+Route::group(['prefix' => 'v01', 'as' => 'v01'], function () {
+    
+    Route::group(['prefix' => 'auth', 'as' => 'auth'], function () {
+
+        Route::post('/register', 'App\Http\Controllers\Api\V01\Auth\AuthController@register')->name('register');
+
+        Route::post('/login', 'App\Http\Controllers\Api\V01\Auth\AuthController@login')->name('login');
+
+        Route::post('/logout', 'App\Http\Controllers\Api\V01\Auth\AuthController@logout')->name('logout');
+    
+        Route::middleware('sanctum:api')->get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
+
+    Route::group(['prefix' => 'chanels', 'as' => 'chanels'], function () {
+
+        Route::get('/', 'App\Http\Controllers\Api\V01\Chanel\ChanelController@chanels');
+
+        Route::post('/create', 'App\Http\Controllers\Api\V01\Chanel\ChanelController@create')->name('create');
+
+        Route::put('/edit', 'App\Http\Controllers\Api\V01\Chanel\ChanelController@edit')->name('edit');
+
+        Route::delete('/delete', 'App\Http\Controllers\Api\V01\Chanel\ChanelController@delete')->name('delete');
+    });
 });
