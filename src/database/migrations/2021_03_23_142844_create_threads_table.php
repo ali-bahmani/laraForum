5de7b46c5271424f49ee8e7f0d46bd2ea5f49086
ali-bahmani <alibahmani0@gmail.com>
2021-03-23 19:22:23 +0430

    add threads table

---
+++ src/database/migrations/2021_03_23_142844_create_threads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+
+            $table->foreiginId('user_id')->constrained()->onDeleted('casecade');
+            $table->foreiginId('chanel_id')->constrained()->onDeleted('casecade');
+
+            $table->unsignedBigInteger('besy_answer_id')->nullable();
+            $table->boolean('flag')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
